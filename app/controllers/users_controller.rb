2674afd663a5107f925e5f169e20f72fd915f7ea
eresh1 <eresh1@umbc.edu>
2012-05-09 19:41:35 -0400

    66 Passing

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,103 @@
+class UsersController < ApplicationController
+  	before_filter :authenticate, :except => [:show, :new, :create]
+  	before_filter :correct_user, :only => [:edit, :update]
+  	before_filter :admin_user,   :only => :destroy
+	before_filter :signed_in_user, :only => [:new, :create] 
+
+  	
+	def signed_in_user
+		if signed_in?
+		redirect_to(root_path)
+  	end
+	end
+
+  def index
+    @title = "All users"
+    @users = User.paginate(:page => params[:page])
+  end
+
+  def show
+    @user = User.find(params[:id])
+        @microposts = @user.microposts.paginate(:page => params[:page])
+    @title = @user.name
+  end
+
+	def new
+	@user = User.new
+	@title = "Sign up"
+	end
+  
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      @title = "Sign up"
+      	#Chapt8 Exercise 2
+      	@user.password = ""
+      	@user.password_confirmation = ""
+      	#End Exercise 2
+
+      render 'new'
+      
+	
+    end
+ end
+  
+  def edit 
+	#@user = User.find(params[:id])
+	@title = "Edit user"
+  end
+
+ 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated."
+      redirect_to @user
+    else
+      @title = "Edit user"
+      render 'edit'
+    end
+end
+
+  def destroy
+    @user.destroy
+    redirect_to users_path, :flash => { :success => "User destroyed." }
+  end
+
+  def following
+    show_follow(:following)
+  end
+
+  def followers
+    show_follow(:followers)
+  end
+
+  def show_follow(action)
+    @title = action.to_s.capitalize
+    @user = User.find(params[:id])
+    @users = @user.send(action).paginate(:page => params[:page])
+    render 'show_follow'
+  end
+  
+
+private
+
+    
+     def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+
+
+    def admin_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) if !current_user.admin? || current_user?(@user)
+    end
+end
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp31::Application.config.secret_token = 'e1d35c82bfb50fd54b6a220e617d538c3b1cb229030b1c3f914d7ab286e0b6e5fda78d9cad7b8b4d9248967cb8c294594f97cf5a70bd7eed2508a7b278e605e7'
